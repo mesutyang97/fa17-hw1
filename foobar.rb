c23c51035129f057e4fc4bccabd14535e92af030
Xiaocheng Yang (Mesut) <mesut.yang97@gmail.com>
2017-09-18 18:27:04 -0700

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,12 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    sum = 0
+    b = a.select { |x| x.to_i.even?}.map{|i| i.to_i + 2}.select{|y| y <= 10}
+    b.each do |e|
+      sum = sum + e
+    end
+    return sum
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,18 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  (a+b)*(a+b)
 
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a.sort.map{|i| i + 1}
 end
+
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  return "#{first_name} #{last_name}"
 end
 
 def blockin_time(a)
